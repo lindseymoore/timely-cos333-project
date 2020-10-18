9397f143a21fd1334189baf4bde90ca40ef1d3fd
Jorge Zreik <jfzreik@gmail.com>
2020-10-18 02:08:00 -0400

    Disabling sqlA modification tracking.

--- timely/__init__.py
+++ timely/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = environ["POSTGRES_URL"]
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 # pylint: disable=wrong-import-position
