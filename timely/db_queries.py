d1bb278f5d880d9040bbe94f1123cb6a8fbe360c
Jorge Zreik <jfzreik@gmail.com>
2020-10-22 23:42:52 -0400

    Add id to class dict.

--- timely/db_queries.py
+++ timely/db_queries.py
@@ -21,7 +21,8 @@ def fetch_class_list(username: str) -> List[dict]:
     class_details = db.session.query(Class).filter(Class.username == username).all()
     for course in class_details:
         # Create class_obj dictionary with all columns that will be displayed to the user
-        class_obj = {"title": course.title, "dept": course.dept, "num": course.num, "color": course.color}
+        class_obj = {"id": course.class_id, "title": course.title, "dept": course.dept,
+                    "num": course.num, "color": course.color}
         classes.append(class_obj)
 
     return classes
