7d65f2c7c454cfe7079f34e1a2a29e7611393840
David Lipman <davidlipman@Davids-MacBook-Pro.local>
2020-12-06 22:09:07 -0800

    Created insert_canvas_key function in db_updates.py to avoid modifying database directly in api.py.

--- timely/db_updates.py
+++ timely/db_updates.py
@@ -1,7 +1,7 @@
 """Functions to update the database."""
 
 from timely import db
-from timely.models import Class, Task, TaskIteration
+from timely.models import Class, Task, TaskIteration, User
 
 
 def mark_task_complete(task_id: int, iteration: int, username: str):
@@ -49,3 +49,18 @@ def delete_iteration(task_id: int, iteration: int):
     for i in next_iterations:
         i.iteration = i.iteration - 1
     db.session.commit()
+
+
+def insert_canvas_key(username: str, api_key: str):
+    """
+    Inserts the Canvas API key for a given user into the database, or updates the API key for a
+    given user if they have already inputted one.
+    """
+    user = db.session.query(User).filter(User.username == username).first()
+    if user is None:
+        new_user = User(username = username, api_key = api_key)
+        db.session.add(new_user)
+        db.session.commit()
+    else:
+        user.api_key = api_key
+        db.session.commit()
